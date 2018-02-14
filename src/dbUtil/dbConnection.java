b906ba1cb6c490d82963353f77d0f8c515af5fbb
samPuriwat <sam.puriwat@gmail.com>
2018-02-14 11:10:29 +0700

    Connected to DB

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static final String SQCONN = "jdbc:school.sqlite";
+    private static final String SQCONN = "jdbc:sqlite:school.sqlite";
 
     public static Connection getConnection() throws SQLException {
         try {
@@ -16,5 +16,5 @@ public class dbConnection {
         }
         return null;
     }//getConnection
-    
+
 }//class
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
-}
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private loginModel model = new loginModel();
+    //import object from scene builder
+    @FXML
+    private Label dbStatus;
+    @FXML
+    private Label loginStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.model.isDatabaseConnection()) {
+            this.dbStatus.setText("Connected to DB");
+        } else {
+            this.dbStatus.setText("Not Connect to DB");
+        }
+
+    }//initialize
+
+}//class
