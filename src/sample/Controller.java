d9dbc284d66eecb2b60fbe578968331cb2cffd9b
samPuriwat <sam.puriwat@gmail.com>
2018-02-14 11:49:20 +0700

    Login and Dashboard

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,19 @@
 package sample;
 
+import Admin.adminController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -33,5 +40,44 @@ public class Controller implements Initializable {
         }
 
     }//initialize
+
+    @FXML
+    public  void Login(ActionEvent event){
+//        System.out.println(username.getText());
+//        System.out.println(password.getText());
+
+        try {
+            if (this.model.isLogin(username.getText(), password.getText())) {
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+                //loginStatus.setText("Welcome to Our System");
+                adminDashBoard();
+            } else {
+                loginStatus.setText("Your username or password is invalid.");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }//Login
+
+    private void adminDashBoard() {
+        try {
+            Stage adminstage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = (Pane) loader.load(getClass().getResource("/admin/adminDashBoard.fxml").openStream());
+            adminController admincontroller = new adminController();
+
+            Scene scene = new Scene(root);
+            adminstage.setScene(scene);
+            adminstage.setResizable(false);
+            adminstage.setTitle("DashBoard");
+            adminstage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+
+    }
 
 }//class
